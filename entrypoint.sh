057dfae9ef57d8e536db05a479d049379b2d195e
ludovicm67 <ludovicmuller1@gmail.com>
2020-02-27 14:50:12 +0100

    entrypoint: use private key if provided through the CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,20 @@ EOF
 PRIVKEY_PATH="$HOME/keys/signing.rsa"
 PUBKEY_PATH="$PRIVKEY_PATH.pub"
 
+# check if a private key is provided through the CI
+if [ -f "$PRIVKEY" ]; then
+  echo "Private key is defined, use it!"
+  cp "$PRIVKEY" "$PRIVKEY_PATH"
+fi
+
+# generate a private key if it doesn't exist
 if ! [ -f "$PRIVKEY_PATH" ]; then
+  echo "Generating a new private key…"
   mkdir -p "$HOME/keys"
   openssl genrsa -out "$PRIVKEY_PATH" 2048
 fi
 
+# generate the public key in all cases using the private key
 openssl rsa -in "$PRIVKEY_PATH" -pubout -out "$PUBKEY_PATH"
 
 sudo cp -v "$PUBKEY_PATH" /etc/apk/keys/
